5b539fb5ef08a67f5f9fa3b40d2b43db74c65abf
vikashkumar18 <kumar.vikash.1539@gmail.com>
2016-10-17 01:53:40 +0530

    removed authentication

--- config.js
+++ config.js
@@ -15,12 +15,12 @@ var config = {
   },
   host: 'http://localhost:3000',
   facebook: {
-    appID: '182364318631927',
-    appSecret: '8df470acd0ff17d40b8b1a3fd8a9daf1',
+    appID: '',
+    appSecret: '',
   },
   google: {
-    clientID: '214969267854-lo4ap4ee3v83qigdgeksdnrp8gkb66bn.apps.googleusercontent.com',
-    clientSecret: 'r8dl_12n9G0f81L1St_KHB7I'
+    clientID: '',
+    clientSecret: ''
   },
     /*Own authentication logic
       The keylen Integer is the size of our hash that comes back and randomSize is the size of the salt.
